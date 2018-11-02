56555cb422be4fa198c3dc17bc7eb7ca7fefad53
cstinepham <cstinepham@gmail.com>
2018-11-02 15:24:53 -0700

    Implement LinkedList

--- HashMap & LRU Cache/skeleton/lib/p04_linked_list.rb
+++ HashMap & LRU Cache/skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,8 +21,17 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
+
+
 
   def [](i)
     each_with_index { |link, j| return link if i == j }
@@ -29,30 +39,68 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+    return false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    last_node = @tail.prev
+
+    last_node.next = new_node
+    new_node.prev = last_node
+
+    @tail.prev = new_node
+    new_node.next = @tail
   end
 
   def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+      end
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+      end
+    end
   end
 
   def each
+    node = @head.next
+    until node == @tail
+      yield(node)
+
+      node = node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
