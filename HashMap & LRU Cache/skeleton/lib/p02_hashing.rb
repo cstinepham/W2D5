97eb39180a965b439acc34659134ee61c88ed0ab
cstinepham <cstinepham@gmail.com>
2018-11-02 14:44:23 -0700

    Stuck on hash functions, move on

--- HashMap & LRU Cache/skeleton/lib/p02_hashing.rb
+++ HashMap & LRU Cache/skeleton/lib/p02_hashing.rb
@@ -3,12 +3,21 @@ class Fixnum
 end
 
 class Array
+
   def hash
+    result = self[0].hash
+    self[1..-1].each do |el|
+        result = result ^ el.hash
+    end
+
+    result
   end
+
 end
 
 class String
   def hash
+    self.bytes.hash
   end
 end
 
@@ -16,6 +25,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sorted_hash = self.sort_by { |k, v| k }
+
+    result = sorted_hash.first.hash
+    sorted_hash[1..-1].each do |pair|
+      result = result ^ pair.hash
+    end
+    result
   end
 end
