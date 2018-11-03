200ab2594d1a5925f08ae1b8600e2e2081c5d108
cstinepham <cstinepham@gmail.com>
2018-11-02 18:09:19 -0700

    end-of-day commit, stuck at LRUCache

--- HashMap & LRU Cache/skeleton/lib/p06_lru_cache.rb
+++ HashMap & LRU Cache/skeleton/lib/p06_lru_cache.rb
@@ -14,6 +14,12 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      update_node!(key, @prc)
+    else
+      add_uncached_key!(key)
+    end
+
   end
 
   def to_s
@@ -22,14 +28,36 @@ class LRUCache
 
   private
 
-  def calc!(key)
+  def add_uncached_key!(key, &prc)
     # suggested helper method; insert an (un-cached) key
+    unless @map.include?(key)
+      @store.append(key, prc.call(key))
+
+      node = Node.new(key, prc.call(key))
+      @map.set(key, node)
+    end
   end
 
-  def update_node!(node)
+  def update_node!(key)
     # suggested helper method; move a node to the end of the list
+    if @map.include?(key)
+      current_node = @map.get(key)
+
+      prev_node = current_node.prev
+      next_node = current_node.next
+
+      prev_node.next = next_node
+      next_node.prev = prev_node
+
+      @map.append(key, nil)
+    end
   end
 
   def eject!
+      head = @store.head
+      first_node = head.next
+      next_node = first_node.next
+      head.next = next_node
+      next_node.prev = head
   end
 end
